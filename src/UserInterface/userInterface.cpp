d79aac381420d5f465d098a908a1c1fded8f09c9
Agustín de Vedia <adevedia@fi.uba.ar>
2023-05-03 18:56:32 -0300

    Added screens

--- src/UserInterface/userInterface.cpp
+++ src/UserInterface/userInterface.cpp
@@ -17,11 +17,19 @@
 
 //=====[Declaration of private data types]=====================================
 
+typedef enum {
+    MAIN_SCREEN,
+    DATE_AND_TIME_CONFIG,
+    PASSWORD_CONFIG,
+    PASSWORD_REQUIRED
+} ScreenStatus_t;
+
 //=====[Declaration and initialization of private global variables]============
 
 LiquidCrystal_I2C lcd(I2C_ADDR, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-
+static ScreenStatus_t screenStatus;
 static int timeIncrement_ms;
+
 
 //=====[Declarations (prototypes) of private functions]========================
 
@@ -37,10 +45,54 @@ static void userInterfaceDisplayInit(int dt);
  */
 static void userInterfaceDisplayUpdate();
 
+/**
+ * @brief Updates the main screen on the display.
+ * 
+ * 23:42       03/05/23
+ * WATER NOT FLOWING
+ * TRAY STANDING STILL
+ * POSITION X  VOL: 98%
+ */
+static void mainScreenUpdate();
+
+/**
+ * @brief Updates the Date And Time Config screen on the display.
+ * 
+ * DATE AND TIME CONFIG
+ * PLEASE ENTER NEW    
+ * DATE AND TIME:      
+ * __:__:__  __/__/____
+ * 
+ */
+static void dateAndTimeConfigScreenUpdate();
+
+/**
+ * @brief Updates the Password Config screen on the display.
+ * 
+ *   PASSWORD  CONFIG  
+ *     PLEASE ENTER    
+ *     NEW PASSWORD    
+ * -------______-------
+ * 
+ */
+static void passwordConfigScreenUpdate();
+
+/**
+ * @brief Updates the Password Required screen on the display.
+ * 
+ * THIS  CONFIGURATION 
+ * IS PROTECTED, PLEASE
+ * ENTER YOUR PASSWORD:
+ * -------______-------
+ * 
+ */
+static void passwordRequiredScreenUpdate();
+
 //=====[Implementations of public functions]===================================
 
 void userInterfaceInit(int dt)
 {
+    screenStatus = MAIN_SCREEN;
     userInterfaceDisplayInit(dt);
 }
 
@@ -67,19 +119,42 @@ static void userInterfaceDisplayUpdate()
     if(accumulatedDisplayTime >= DISPLAY_REFRESH_TIME_MS) {
         accumulatedDisplayTime = 0;
 
+        switch(screenStatus) {
+            case MAIN_SCREEN:
+                mainScreenUpdate();
+                break;
+            case DATE_AND_TIME_CONFIG:
+                dateAndTimeConfigScreenUpdate();
+                break;
+            case PASSWORD_CONFIG:
+                passwordConfigScreenUpdate();
+                break;
+            case PASSWORD_REQUIRED:
+                passwordRequiredScreenUpdate();
+                break;
+            default:
+                userInterfaceInit(timeIncrement_ms);
+        }
+    } else {
+        accumulatedDisplayTime += timeIncrement_ms;
+    }
+}
+
+static void mainScreenUpdate()
+{
         lcd.setCursor(0,1);
         if(isWaterFlowing())
-            lcd.print("Water flowing.    ");
+            lcd.print("WATER FLOWING.    ");
         else
-            lcd.print("Water not flowing.");
+            lcd.print("WATER NOT FLOWING.");
         
         lcd.setCursor(0,2);
         if(isTrayInPlace())
-            lcd.print("Tray standing still.");
+            lcd.print("TRAY STANDING STILL.");
         else if (isTrayMovingUp())
-            lcd.print("Tray moving up.     ");
+            lcd.print("TRAY MOVING UP.     ");
         else if(isTrayMovingDown())
-            lcd.print("Tray moving down.   ");
+            lcd.print("TRAY MOVING DOWN.   ");
 
         /*
         * #TODO:
@@ -89,7 +164,7 @@ static void userInterfaceDisplayUpdate()
         */
        if(isTrayInPlace()) {
             lcd.setCursor(0,3);
-            lcd.print("Position X");
+            lcd.print("POSITION X  ");
         } else {
             lcd.setCursor(0,3);
             lcd.print("          ");
@@ -97,15 +172,78 @@ static void userInterfaceDisplayUpdate()
 
         // #TODO: Show % of water left.
         lcd.setCursor(12,3);
-        lcd.print("Vol: 98%");
+        lcd.print("VOL: 98%");
         
         // #TODO: Date & Time - Update
         lcd.setCursor(0,0);
-        lcd.print("18:15");
+        lcd.print("18:15       ");
         lcd.setCursor(12,0);
         lcd.print("03/05/23");
+}
 
-    } else {
-        accumulatedDisplayTime += timeIncrement_ms;
-    }
+static void dateAndTimeConfigScreenUpdate()
+{
+    lcd.setCursor(0,0);
+    lcd.print("DATE AND TIME CONFIG");
+
+    lcd.setCursor(0,1);
+    lcd.print("PLEASE ENTER NEW    ");
+
+    lcd.setCursor(0,2);
+    lcd.print("DATE AND TIME:      ");
+
+    lcd.setCursor(0,3);
+    lcd.print("  :  :      /  /    ");
+
+    delay(2000);
+
+    lcd.setCursor(0,1);
+    lcd.print("NEW  DATE  AND  TIME");
+
+    lcd.setCursor(0,2);
+    lcd.print("       SAVED!       ");
+
+    lcd.setCursor(0,3);
+    lcd.print("18:47:00  03/05/2023");    
+}
+
+static void passwordConfigScreenUpdate()
+{
+    lcd.setCursor(0,0);
+    lcd.print("  PASSWORD  CONFIG  ");
+
+    lcd.setCursor(0,1);
+    lcd.print("    PLEASE ENTER    ");
+
+    lcd.setCursor(0,2);
+    lcd.print("    NEW PASSWORD    ");
+
+    lcd.setCursor(0,3);
+    lcd.print("-------      -------");
+
+    delay(2000);
+
+    lcd.setCursor(0,1);
+    lcd.print("  PASSWORD UPDATED  ");
+
+    lcd.setCursor(0,2);
+    lcd.print("      CORRECTLY     ");
+
+    lcd.setCursor(0,3);
+    lcd.print("-------******-------");
+}
+
+static void passwordRequiredScreenUpdate()
+{
+    lcd.setCursor(0,0);
+    lcd.print("THIS  CONFIGURATION ");
+
+    lcd.setCursor(0,1);
+    lcd.print("IS PROTECTED, PLEASE");
+
+    lcd.setCursor(0,2);
+    lcd.print("ENTER YOUR PASSWORD:");
+
+    lcd.setCursor(0,3);
+    lcd.print("-------      -------");
 }
